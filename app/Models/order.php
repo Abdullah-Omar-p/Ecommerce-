ebc7e9f6a60fc857b07bdbfa22c0f747325ef401
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2022-12-04 16:53:03 +0200

    retu.

--- app/Models/order.php
+++ app/Models/order.php
@@ -8,5 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     use HasFactory;
-    //here we will add some things 
 }
--- app/Models/photo.php
+++ app/Models/photo.php
@@ -8,5 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class photo extends Model
 {
     use HasFactory;
-    //wefshzuis
 }
