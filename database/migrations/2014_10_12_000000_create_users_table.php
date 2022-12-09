0cd27f0f768605dcf0f40edea14e8bdffcdc47c5
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2022-12-09 14:00:53 +0200

    comment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        // do it ya falla7 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstname')->nullable();
